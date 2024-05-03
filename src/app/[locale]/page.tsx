afeeafd237d942b10ddee756e3d5a50efe3860c5
HusseinOklah <45362170+HusseinOklah@users.noreply.github.com>
2024-05-03 19:52:08 +0000

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import SliderContainer from "../../components/SliderContainer";
 
 export default function page() {
     return (<>
+        <h1>Hello I am Here</h1>
         <SliderContainer />
     </>);
 }
