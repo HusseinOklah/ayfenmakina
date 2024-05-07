b727e4a984c6d07795f4c5da01c566ea3604b0e1
HusseinOklah <45362170+HusseinOklah@users.noreply.github.com>
2024-05-07 21:32:34 +0300

    Update page.tsx

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -3,17 +3,10 @@ import { motion } from "framer-motion"
 const list = { hidden : {opacity:0}}
 const item = {hidden :{x:-10, opacity:0}}
 export default function page(){
-    
-
-
-
-return (
-  <motion.ul animate="hidden" variants={list}>
-      <motion.li variants={item} />
-          <motion.li variants={item} />
-              <motion.li variants={item} />
-                </motion.ul>
-                )
-
-  
-}
+    return (
+        <motion.ul animate="hidden" variants={list}>
+            <motion.li variants={item} />
+            <motion.li variants={item} />
+            <motion.li variants={item} />
+        </motion.ul>
+    )}
